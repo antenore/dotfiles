f2dd051022d7e34b70085b990bc86a6dceca46a2
Antenore Gatta <antenore@simbiosi.org>
2013-02-19 22:28:13 +0100

    binaries

---
+++ initscreen.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Example xrandr multiscreen init
+
+#xrandr --output LVDS --auto
+#xrandr --output VGA --auto --right-of LVDS
+
+xrandr --output HDMI1 --off --output LVDS1 --mode 1600x900 --pos 160x1080 \
+       --rotate normal --output DP1 --off --output VGA1 --mode 1920x1080  \
+       --pos 0x0 --rotate normal
+
