f65f8655bd26fad9d18770006ae6547502d43bdd
Antenore Gatta <antenore@simbiosi.org>
2013-07-18 08:08:48 +0200

    Fiyed spectrwm baraction width and height

--- baraction.sh
+++ baraction.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 #conky | dzen2 -fg "#c8e7a8" -bg "#1a1a1a" -ta right -h 14 -x 900 -y 3 \
-conky | dzen2 -fg "#c8e7a8" -bg "#1a1a1a" -ta right -h 14 \
+conky | dzen2 -fg "#c8e7a8" -bg "#1a1a1a" -ta right -h 14 -x 900 -y 3 \
               -fn -*-tamsyn-medium-*-*-*-12-*-*-*-*-*-*-*
 exit 0
