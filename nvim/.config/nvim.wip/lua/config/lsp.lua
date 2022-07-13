660ff463e9380e0263371dfa721e26ad98f2e0f5
Antenore Gatta (tmow) <antenore@simbiosi.org>
2022-07-13 02:02:49 +0200

    refactor: improving lsp mapping and cleanup

--- nvim/.config/nvim.wip/lua/config/lsp.lua
+++ nvim/.config/nvim.wip/lua/config/lsp.lua
@@ -1,15 +1,16 @@
+require "utils"
 local util = require 'lspconfig/util'
 -- {{{ ===== lsp-colors.nvim ====================================================
 require("lsp-colors").setup({})
 -- }}}
 -- {{{ ===== nvim-lsp-installer =================================================
 require("nvim-lsp-installer").setup({
-    automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
+    automatic_installation = true,
     ui = {
         icons = {
             server_installed = "✓",
             server_pending = "➜",
-            server_uninstalled = "✗"
+            server_uninstalled = "✗",
         }
     }
 })
@@ -17,69 +18,59 @@ require("nvim-lsp-installer").setup({
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
-vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+nnoremap {'<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', silent=true }
+nnoremap {'[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', silent=true }
+nnoremap {']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', silent=true }
+nnoremap {'<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', silent=true }
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+local on_attach = function(_, bufnr)
+	-- Enable completion triggered by <c-x><c-o>
+	-- vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopt = { buffer=bufnr, silent=true }
+	nnoremap { 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>',bufopt }
+	nnoremap { 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>',bufopt }
+	nnoremap { 'K', '<cmd>lua vim.lsp.buf.hover()<CR>',bufopt }
+	nnoremap { 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>',bufopt }
+	nnoremap { '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>',bufopt }
+	nnoremap { '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>',bufopt }
+	nnoremap { '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>',bufopt }
+	nnoremap { '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',bufopt }
+	nnoremap { '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>',bufopt }
+	nnoremap { '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>',bufopt }
+	nnoremap { '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>',bufopt }
+	nnoremap { 'gr', '<cmd>lua vim.lsp.buf.references()<CR>',bufopt }
+	nnoremap { '<leader>f','<cmd>lua vim.lsp.buf.formatting()<CR>',bufopt }
 end
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-require'lspconfig'.cmake.setup{
-  on_attach = on_attach,
+
+local lspconfig = require('lspconfig')
+
+local servers = { 'cmake', 'jsonls', 'solargraph', 'eslint', 'tsserver', 'vimls', 'bashls', 'marksman' }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+lspconfig.clangd.setup{
+  -- on_attach = on_attach,
   capabilities = capabilities
 }
--- It crashes
--- require'lspconfig'.ccls.setup {
---   on_attach = on_attach,
---   capabilities = capabilities,
---   cmd = { "ccls" },
---   init_options = {
---     compilationDatabaseDirectory = "build";
---     index = {
---       threads = 0;
---       };
---     clangd = {
---       excludeArgs = { "-frounding-math"},
---       }
---     }
--- }
-require'lspconfig'.jsonls.setup{
-  on_attach = on_attach,
+
+require("clangd_extensions").setup{
+  -- on_attach = on_attach,
   capabilities = capabilities
 }
--- Ruby
-require'lspconfig'.solargraph.setup{
-  on_attach = on_attach,
-  capabilities = capabilities
-}
--- require'lspconfig'.sorbet.setup{
---   on_attach = on_attach,
---   capabilities = capabilities
--- }
+
 -- Lua
-require'lspconfig'.sumneko_lua.setup{
+lspconfig.sumneko_lua.setup{
   on_attach = on_attach,
   capabilities = capabilities,
   settings = {
@@ -105,36 +96,19 @@ require'lspconfig'.sumneko_lua.setup{
       },
     },
 }
-require'lspconfig'.clangd.setup{
-  -- on_attach = on_attach,
-  capabilities = capabilities
-}
-require("clangd_extensions").setup{
-  -- on_attach = on_attach,
-  capabilities = capabilities
-}
---- require'lspconfig'.tsserver.setup{}
-require'lspconfig'.eslint.setup{
+
+lspconfig.prosemd_lsp.setup{
   on_attach = on_attach,
-  capabilities = capabilities
+  capabilities = capabilities,
+    cmd = { "/home/antenore/.cargo/bin/prosemd-lsp", "--stdio" },
+    filetypes = { "markdown" },
+    root_dir = function(fname)
+      return util.find_git_ancestor(fname) or vim.fn.getcwd()
+    end,
+    settings = {},
 }
-require'lspconfig'.tsserver.setup{
-  on_attach = on_attach,
-  capabilities = capabilities
-}
-require'lspconfig'.vimls.setup{
-  on_attach = on_attach,
-  capabilities = capabilities
-}
-require'lspconfig'.bashls.setup{
-  on_attach = on_attach,
-  capabilities = capabilities
-}
-require'lspconfig'.marksman.setup{
-  on_attach = on_attach,
-  capabilities = capabilities
-}
-require'lspconfig'.puppet.setup {
+
+lspconfig.puppet.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   -- DEBUG add '--stdio', '--debug=/tmp/puppet-lsp-debug.log'
@@ -149,6 +123,7 @@ require'lspconfig'.puppet.setup {
     return util.root_pattern(unpack(root_files))(fname) or util.path.dirname(fname)
   end,
 }
+
 -- Enable diagnostics
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
@@ -157,4 +132,3 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     update_in_insert = true,
   }
 )
-
--- nvim/.config/nvim.wip/lua/config/nvim-tree.lua
+++ nvim/.config/nvim.wip/lua/config/nvim-tree.lua
@@ -1,5 +1,5 @@
 map { "<F10>", ":NvimTreeToggle<CR>" }
-nnoremap { "<leader>f", ":NvimTreeToggle<CR>" }
+-- nnoremap { "<leader>f", ":NvimTreeToggle<CR>" }
 -- NvimTreeOpen and NvimTreeClose are also available if you need them
 -- a list of groups can be found at `:help nvim_tree_highlight`
 -- vim.cmd [[highlight NvimTreeFolderIcon guifg=#1b95e0]]
